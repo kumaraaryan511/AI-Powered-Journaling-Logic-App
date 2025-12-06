a51f83797b7fe565c00946c4a24a46eff5bfa3fc
kumaraaryan511 <108775073+kumaraaryan511@users.noreply.github.com>
2025-12-06 15:31:42 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import torch
 import sys
 model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 
+import warnings
+import logging
+warnings.filterwarnings("ignore")
+logging.getLogger("transformers").setLevel(logging.ERROR)
+
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
