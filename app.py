b620327e2c828baf65b4373b3bdd482bc02c53ca
kumaraaryan511 <108775073+kumaraaryan511@users.noreply.github.com>
2025-12-06 15:05:55 -0800

    using roberta model for text analysis

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 import sys
-import re
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sia = SentimentIntensityAnalyzer()
-def analyze_sentence(text: str) -> float:
-    if not text.strip():
-        return 0.0
-    lowered = text.lower()
-    scores = sia.polarity_scores(text)
-    return scores["compound"]
-    
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python wellbeing.py \"your text\"")
-        sys.exit(1)
-    sentence = " ".join(sys.argv[1:])
-    score = analyze_sentence(sentence)
-    print(score)
+model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+def score(text):
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = model(**inputs)
+    probs = torch.nn.functional.softmax(outputs.logits, dim=1)[0]
+
+    return float(probs[2] - probs[0])
+
+
+
+text = " ".join(sys.argv[1:])
+print(score(text))
+
+
